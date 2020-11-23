c23b2841f95e86cf2d61c60f3772e33429c23631
kdi0905 <sdo5895@naver.com>
2020-11-23 14:27:12 +0900

    lombok 적용

--- fileuploadtest/src/main/java/gd/fintech/fileuploadtest/vo/Board.java
+++ fileuploadtest/src/main/java/gd/fintech/fileuploadtest/vo/Board.java
@@ -2,41 +2,14 @@ package gd.fintech.fileuploadtest.vo;
 
 import java.util.List;
 
+import lombok.Data;
 
+@Data
 public class Board {
 	private int boardId;
 	private String boardTitle;
 	private String boardContent;
 	private List<Boardfile> boardfiles;
 	
-	public int getBoardId() {
-		return boardId;
-	}
-	public void setBoardId(int boardId) {
-		this.boardId = boardId;
-	}
-	public String getBoardTitle() {
-		return boardTitle;
-	}
-	public void setBoardTitle(String boardTitle) {
-		this.boardTitle = boardTitle;
-	}
-	public String getBoardContent() {
-		return boardContent;
-	}
-	public void setBoardContent(String boardContent) {
-		this.boardContent = boardContent;
-	}
-	public List<Boardfile> getBoardfiles() {
-		return boardfiles;
-	}
-	public void setBoardfiles(List<Boardfile> boardfiles) {
-		this.boardfiles = boardfiles;
-	}
-	@Override
-	public String toString() {
-		return "Board [boardId=" + boardId + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
-				+ ", boardfiles=" + boardfiles + "]";
-	}
 	
 }
--- fileuploadtest/src/main/java/gd/fintech/fileuploadtest/vo/VoTest.java
+++ fileuploadtest/src/main/java/gd/fintech/fileuploadtest/vo/VoTest.java
@@ -1,23 +1,11 @@
 package gd.fintech.fileuploadtest.vo;
 
+import lombok.Data;
+
+@Data
 public class VoTest {
 	private int x;
 	private int y;
-	public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	@Override
-	public String toString() {
-		return "VoTest [x=" + x + ", y=" + y + "]";
-	}
+	
 	
 }
